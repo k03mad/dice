79cb1b77de0a834fc73e4451fec0fe11997de91d
k03mad <k03.mad@gmail.com>
2024-08-09 17:05:23 +0300

    remove lodash

--- app/run.js
+++ app/run.js
@@ -5,10 +5,13 @@ import {emitKeypressEvents} from 'node:readline';
 import {log} from '@k03mad/simple-log';
 import chalk from 'chalk';
 import {globby} from 'globby';
-import _ from 'lodash';
 import image from 'terminal-image';
 
-const {green, dim} = chalk;
+const {green, dim, bold} = chalk;
+
+const getRandomArrElem = arr => arr[
+    Math.floor(Math.random() * arr.length)
+];
 
 const DICE_PICTURES_PATH = 'app/png';
 const DICE_HEIGHT = '30%';
@@ -18,23 +21,22 @@ const EXIT_KEY = 'q';
 const texts = {
     message: green([
         '— enter any digit to generate this count of dices',
-        '— press any key to repeat previous generating',
-        `— press "CTRL+C" or enter "${EXIT_KEY}" to exit`,
+        '— press any key to repeat previous count generating',
+        bold(`— press "CTRL+C" or enter "${EXIT_KEY}" to exit`),
     ].join('\n')),
     delimiter: dim('——————————————\n'),
 };
 
 const imgs = await globby(DICE_PICTURES_PATH);
+log(texts.message);
+
+let currentCount = 1;
 
 emitKeypressEvents(process.stdin);
 
 if (process.stdin.isTTY) {
     process.stdin.setRawMode(true);
 }
-
-log(texts.message);
-
-let count = 1;
 
 process.stdin.on('keypress', async (str, key) => {
     if (
@@ -47,13 +49,17 @@ process.stdin.on('keypress', async (str, key) => {
     const inputNum = Number(str);
 
     if (inputNum) {
-        count = inputNum;
+        currentCount = inputNum;
     }
 
     log(texts.delimiter);
 
-    for (let i = 0; i < count; i++) {
-        const img = await image.file(_.sample(imgs), {height: DICE_HEIGHT});
+    for (let i = 0; i < currentCount; i++) {
+        const img = await image.file(
+            getRandomArrElem(imgs),
+            {height: DICE_HEIGHT},
+        );
+
         log(img);
     }
 });
