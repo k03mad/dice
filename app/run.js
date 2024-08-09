2a1d38582791a892269f8540bb8f98efd2819e6f
k03mad <k03.mad@gmail.com>
2024-08-09 17:31:48 +0300

    pretty

--- app/run.js
+++ app/run.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
+import fs from 'node:fs/promises';
+import path from 'node:path';
 import {emitKeypressEvents} from 'node:readline';
 
 import {log} from '@k03mad/simple-log';
-import {globby} from 'globby';
 import image from 'terminal-image';
 
 import {
@@ -16,7 +17,8 @@ import {
 } from './utils/config.js';
 import {getRandomArrElem} from './utils/helpers.js';
 
-const imgs = await globby(DICE_PICTURES_PATH);
+const imgFiles = await fs.readdir(DICE_PICTURES_PATH);
+const imgPaths = imgFiles.map(img => path.join(DICE_PICTURES_PATH, img));
 
 emitKeypressEvents(process.stdin);
 
@@ -28,20 +30,20 @@ let diceCurrentCount = DICE_DEFAULT_COUNT;
 log(texts.message);
 
 process.stdin.on('keypress', async (char, key) => {
-    log(texts.delimiter);
-
     if (
         (key.ctrl === true && key.name === EXIT_CTRL_MODIFIER)
         || char === EXIT_EXTRA_KEY
     ) {
         process.exit();
+    } else {
+        log(texts.delimiter);
     }
 
     diceCurrentCount = Number(char) || diceCurrentCount;
 
     const output = await Promise.all(
         Array.from({length: diceCurrentCount}, () => {
-            const diceImg = getRandomArrElem(imgs);
+            const diceImg = getRandomArrElem(imgPaths);
             return image.file(diceImg, {height: DICE_HEIGHT});
         }),
     );
